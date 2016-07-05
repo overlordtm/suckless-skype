0fb45d0806cc10d609c2c8f406ea3541d3c454d4
Andraz Vrhovec <andraz@koofr.net>
2016-07-05 14:59:02 +0200

    Changes!!!

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
+# Run
+
 ```
 npm install
 npm start
+```
+
+# Development
+```
+export NWJS_BUILD_TYPE=sdk
+npm install
 ```
--- skype.js
+++ skype.js
@@ -7,6 +7,7 @@ const SETTINGS_BTN_ID = "#menuItem-userSettings";
 
 var gui = require('nw.gui');                  // Load native UI library
 var skypeWin = gui.Window.get();                  // Reference to window and tray
+var statusRefresherId = null;
 
 // prevent duplicate tray on reload (for development)
 window.addEventListener('beforeunload', function() {
@@ -20,7 +21,7 @@ var menu = new nw.Menu();
 var settingsItem = new nw.MenuItem({ type: 'normal', label: 'Settings' })
 var exitItem = new nw.MenuItem({ type: 'normal', label: 'Exit' })
 var availableMenuItem = new gui.MenuItem({ label: 'Available', icon: 'img/available.png' });
-var busyMenuItem = new gui.MenuItem({ label: 'Busy', icon: 'img/busy.png' });
+var busyMenuItem = new gui.MenuItem({ label: 'Do not disturb', icon: 'img/busy.png' });
 var awayMenuItem = new gui.MenuItem({ label: 'Away', icon: 'img/away.png' });
 var hiddenMenuItem = new gui.MenuItem({ label: 'Hidden', icon: 'img/hidden.png' });
 
@@ -74,10 +75,25 @@ var tray = new nw.Tray({
 // show window on tray click
 tray.on('click', function() {
     console.log("tray click", skypeWin);
-    skypeWin.show();
+    if(!skypeWin.shown) {
+        skypeWin.show();
+        skypeWin.shown = true;
+    } else {
+        skypeWin.hide();
+        skypeWin.shown = false;
+    }
+    
+    
 })
 
 /* setup handlers */
+
+// open urls in new broser
+skypeWin.on('new-win-policy', function(frame, url, policy) {
+  nw.Shell.openExternal(url);
+  policy.ignore();
+});
+
 
 // handle exit commad
 skypeWin.on('close', function() {
@@ -89,23 +105,49 @@ skypeWin.on('close', function() {
     }
 })
 
-skypeWin.on('document-start', function(frame) {
-    console.log("document-start", frame);
-    $(frame.document).ready(function() {
-        var x = $(this).find(SETTINGS_BTN_ID);
-        console.log("frame ready", x, this);
-
-    });
-    // $(SKYPE_FRAME_ID).contents().ready(function () {
-    //     var settingsBtn = skypeFind(SETTINGS_BTN_ID);
-    //     console.log("SKYPE frame ready", settingsBtn);
-    // });
+skypeWin.on('loaded', function(frame) {
+    var modal = $('#settings')
+    if (modal.data('observed') != "true") {
+        modal.one('shown.bs.modal', function (e) {
+            populateSettings(modal);
+            initSettingsListener(modal);
+        })
+        modal.data("observed", "true")
+    }
+    if(!statusRefresherId) {
+        statusRefresherId = setInterval(statusRefresher, 1000);
+    }
 })
 
 /* custom functions */
 
+function statusRefresher() {
+    var status = skypeFind("#meComponent > div > swx-me-area > div > div > div.Me-skyContainer > swx-avatar-deprecated > span");
+    if (!status) {
+        return;
+    }
+    var clazz = status.attr('class');
+    status = null;
+    if(clazz.indexOf('online') > 0) {
+        // online
+        setTrayIcon('available.png');
+    } else if (clazz.indexOf('idle') > 0) {
+        // away
+        setTrayIcon('away.png');
+    } else if (clazz.indexOf('donotdisturb') > 0) {
+        // busy
+        setTrayIcon('busy.png');
+    } else if (clazz.indexOf('offline') > 0) {
+        // invisible
+        setTrayIcon('hidden.png');
+    } else {
+        setTrayIcon('skype.png');
+    }
+    clazz = null;
+}
+
 function setStatus(statusName) {
-    var status = $(skypeFrame()).find(statusName)
+    var status = skypeFind(statusName)
     if (status.length > 0) {
         status[0].click();
     }
@@ -128,38 +170,29 @@ function skypeFindAll(selector) {
 }
 
 function openSettings() {
-    var settingsBtn = skypeFind(SETTINGS_BTN_ID);
-    if(!settingsBtn.hasClass("active")) {
-        settingsBtn[0].click();
-        setTimeout(customizeSettings, 0);
-    }
+    $('#settings').modal();
 }
 
-function customizeSettings() {
-    var list = skypeFind("#swxContent1 > swx-navigation > div > div > section > div.UserSettingsPage-master > ul");
-    if (!list) {
-        console.log("Failed to get settings pane");
-        return
-    }
+function populateSettings(modal) {
+    modal.find('input').each(function () {
+        $this = $(this);
+        var key = $this.data('settings-key');
+        if (key) {
+            var value = localStorage.getItem(key);
+            console.log("populating", $this, value);
+            $this.prop('checked', value === "true");
+        }
+    })
+}
 
-    if (list.hasClass('suckless-skype')) {
-        console.log("Settings already customized");
-        return;
-    }
-
-    console.log("Customizing settings menu");
-
-    var listItem = $('<li>').attr('role', 'option');
-    var href = $('<a>').attr('href', '#').addClass('DesktopSettingsPage-category');
-    var title = $('<h2>').addClass('DesktopSettingsPage-label').html("Desktop");
-
-    $(href).on('click', function() {
-        var detailsPane = skypeFind('.UserSettingsPage-detail');
-        console.log("Desktop settings clicked", detailsPane);
-    });
-
-    href.append(title);
-    listItem.append(href);
-    list.append(listItem);
-    list.addClass('suckless-skype');
+function initSettingsListener(modal) {
+    modal.find('input').each(function () {
+        $(this).on('change', function () {
+            $this = $(this);
+            var key = $this.data('settings-key');
+            var value = $this.prop('checked');
+            localStorage.setItem(key, value);
+            console.log("Settings changed", key, value);
+        })
+    })
 }
